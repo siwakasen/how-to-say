266a45ebc43add133613d4be8646ce84fb8734f8
siwakasen <siwakasen@gmail.com>
2026-04-09 13:30:36 +0700

    feat: update set loading and responsiveness

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ export const Route = createFileRoute('/')({
 
 function App() {
   const [text, setText] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const suggestions = [
     'please',
@@ -20,42 +21,50 @@ function App() {
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter' && text.length !== 0) {
       console.log('Enter key pressed!')
+      setLoading(true)
     }
   }
 
   return (
-    <main className="page-wrap px-4 pb-8 pt-14 flex flex-col items-center text-center">
+    <main className="page-wrap px-4 pb-10 pt-16 flex flex-col items-center text-center">
+
       {/* Title + Input */}
-      <h1 className="text-3xl sm:text-4xl font-bold mb-6">
-        How to say{' '}
+      <h1 className="flex flex-wrap justify-center items-center gap-2 text-xl sm:text-3xl md:text-4xl font-bold mb-6 leading-snug">
+        <span>How to say</span>
         <input
           value={text}
           onChange={(e) => setText(e.target.value)}
           onKeyDown={handleKeyDown}
           placeholder="Any words..."
-          className="text-[30px] outline-none border-b-2 border-(--line) py-4 px-2 bg-transparent"
+          className="min-w-35 max-w-55 sm:max-w-xs md:max-w-sm 
+                     text-xl sm:text-3xl md:text-3xl 
+                     outline-none border-b-2 border-(--line) 
+                     px-2 py-0.5 bg-transparent text-center sm:text-start"
         />
       </h1>
 
-      {/* Suggestions */}
-      <div className="text-amber-300 text-sm mb-6 font-bold">
-        Try:{' '}
-        {suggestions.map((item, i) => (
-          <button
-            key={i}
-            onClick={() => setText(item)}
-            className="underline mr-2 hover:cursor-pointer"
-          >
-            {item},
-          </button>
-        ))}
-      </div>
+      {
+        !loading ? <>      {/* Suggestions */}
+          <div className="max-w-md text-xs sm:text-sm md:text-base text-amber-300 font-semibold mb-6 leading-relaxed">
+            Try:{' '}
+            {suggestions.map((item, i) => (
+              <button
+                key={i}
+                onClick={() => setText(item)}
+                className="underline mr-2 hover:cursor-pointer"
+              >
+                {item},
+              </button>
+            ))}
+          </div>
 
-      {/* Description */}
-      <p className="max-w-xl text-(--sea-ink-soft)">
-        Improve your English pronunciation by listening to examples of real
-        people speaking English on YouTube.
-      </p>
+          {/* Description */}
+          <p className="max-w-md sm:max-w-lg text-sm sm:text-base text-[var(--sea-ink-soft)] leading-relaxed">
+            Improve your English pronunciation by listening to examples of real
+            people speaking English on YouTube.
+          </p>
+        </> : <><p>Searching...</p></>
+      }
     </main>
   )
 }
