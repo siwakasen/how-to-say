a71bd4b9ac93873a2bc2a8eff46df685a54d51c7
siwakasen <siwakasen@gmail.com>
2026-04-10 00:03:14 +0700

    fix: update width video player

--- src/components/YoutubePlayer.tsx
+++ src/components/YoutubePlayer.tsx
@@ -5,19 +5,18 @@ export function YoutubePlayer({
   videoId,
   start,
 }: {
-
   videoId: string;
   start: number;
 }) {
   return (
-    <div className="w-full max-w-160 mx-auto">
+    <div className="w-full">
       {/* <LiteYouTubeEmbed */}
       {/*   id={videoId} */}
       {/*   params={`start=${start}playsinline=1&iv_load_policy=3&rel=0&controls=1&fs=0&autoplay=1&enablejsapi=1`} */}
       {/*   title='' */}
       {/* /> */}
       <iframe
-        className="w-full max-w-160 mx-auto aspect-video"
+        className="w-full max-w-212.5 mx-auto aspect-video"
         src={`https://www.youtube.com/embed/${videoId}?playsinline=1&iv_load_policy=3&rel=0&controls=1&fs=0&start=${start}&autoplay=1&enablejsapi=1`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
