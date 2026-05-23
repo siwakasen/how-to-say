0faf923df9883d677a752b05f0878e8ef4c566cc
siwakasen <siwakasen@gmail.com>
2026-05-23 19:10:39 +0700

    feat: migrate to cloudflare pages for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,40 @@
-import { defineConfig } from 'vite';
-import { devtools } from '@tanstack/devtools-vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-
-import viteReact from '@vitejs/plugin-react';
+import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import { devtools } from '@tanstack/devtools-vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
+import { defineConfig } from 'vite';
 
+const isCF = process.env.CLOUDFLARE === 'true';
 const config = defineConfig({
   plugins: [
+    ...(isCF ? [cloudflare({ viteEnvironment: { name: 'ssr' } })] : []),
     devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    nitro({
+      preset: 'bun',
+      routeRules: {
+        '/assets/**': {
+          headers: {
+            'cache-control': 'public, max-age=31536000, immutable',
+          },
+        },
+        '/**/*.webp': {
+          headers: {
+            'cache-control': 'public, max-age=604800',
+          },
+        },
+        '/**/*.svg': {
+          headers: {
+            'cache-control': 'public, max-age=604800',
+          },
+        },
+      },
+      rollupConfig: { external: [/^@sentry\//] },
+    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] }, preset: 'bun' }),
   ],
 });
 
